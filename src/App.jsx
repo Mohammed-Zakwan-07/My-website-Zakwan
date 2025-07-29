53aca4bdf6ce82f25570abaf1f136aa3a965bce0
Mohammed Zakwan <zakwanmd16@gmail.com>
2025-07-29 23:55:49 +0530

    add something cool

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
-import DoodlePage from "./components/Doodlepage";
+import DoodlePage from "./components/DoodlePage";
 import Doodle from "./components/Doodle";
 
 export default function App() {
